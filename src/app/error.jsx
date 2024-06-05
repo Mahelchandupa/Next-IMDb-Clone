2eceb68c26724b143dd22d417dc9f8a1e3e9c4b4
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-05 21:45:15 +0530

    fix some issue

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react" 
 
-const error = ({ error, reset }) => {
+const Error = ({ error, reset }) => {
 
   useEffect(() => {
     console.log(error)
@@ -16,4 +16,4 @@ const error = ({ error, reset }) => {
   )
 }
 
-export default error
+export default Error
