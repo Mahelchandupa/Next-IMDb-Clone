bdc142be48b2438afe6f706d556126b2c6d1bd9b
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-05 17:51:30 +0530

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=' max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className=' text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto a ea, hic rem aspernatur ratione atque ullam quasi dolores ipsa facilis nulla cupiditate quidem numquam, quo temporibus itaque pariatur totam sequi ipsum cum. Iste consectetur quos veritatis incidunt dolorem cumque vel id laboriosam eum dicta, voluptatibus nobis ut magnam sapiente velit inventore dolorum quo libero aliquid totam eius itaque. Est deleniti eligendi iusto, tenetur totam, aliquam aut velit dolor reprehenderit voluptatibus dolore. Sit sequi, magni labore amet doloremque vitae iure!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga reiciendis dicta eum placeat veniam alias eaque tempora sequi deleniti blanditiis qui eius ex sunt quaerat, deserunt eos asperiores repellendus facilis, molestiae tempore obcaecati? Nemo voluptate expedita quibusdam eos dicta quidem?</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae nesciunt voluptatum magnam sequi dicta. Harum soluta odio sapiente eius qui itaque officiis. Iure neque error placeat natus maiores consequuntur repudiandae accusantium provident officia? Sed qui officia autem, sapiente corporis accusantium, veritatis voluptates est aspernatur numquam eum voluptas doloribus laboriosam repellat aut id a quisquam impedit dignissimos nesciunt aperiam commodi blanditiis omnis. Deserunt saepe optio qui, odio quo consequatur soluta ratione ea fuga debitis? Reiciendis perspiciatis earum dolores quis dolore deleniti nostrum, placeat rem, ut impedit molestiae tempore. Ipsa laboriosam repellat dolorum et assumenda perspiciatis molestiae dicta dignissimos aspernatur corporis est, illo adipisci. Aliquid impedit ab cum veniam tempore inventore nisi, odio, nihil quas, quasi earum enim autem molestias quidem soluta possimus asperiores obcaecati recusandae iure cupiditate consectetur voluptatum. Quia voluptates, tempora minus suscipit fugit iure dolore possimus rem aliquam ut. Ex nulla expedita tempore fugit cupiditate nihil quod, et illo ducimus, itaque veritatis culpa quidem explicabo voluptatibus. Quo id accusantium nesciunt delectus possimus labore fuga sed tempora iusto a maxime impedit velit excepturi repudiandae est in aut obcaecati fugiat sunt, magni nulla alias aliquam. Eaque delectus placeat, quaerat culpa ipsum ea nam, veniam maiores vel atque numquam aspernatur reprehenderit autem.</p>
+    </div>
+  )
+}
+
+export default About
